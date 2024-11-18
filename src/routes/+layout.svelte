0f22906a4f82664a1eb239259d63bf1ad25d3092
Mapagmataas <98333916+Mapagmataas1331@users.noreply.github.com>
2024-11-18 19:08:53 +0700

    Added toaster

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
 	import Header from '$shared/components/Header.svelte';
+	import { Toaster } from '$shared/components/ui/sonner/index.js';
 	import '@/app.scss';
 
 	let { children } = $props();
 </script>
 
 <Header profile={false} />
-{@render children()}
+
+<Toaster />
+
+{@render children?.()}
